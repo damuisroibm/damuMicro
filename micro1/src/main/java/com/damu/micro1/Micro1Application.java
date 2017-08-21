39a09a30f34ed5090308cf259326e35765723f36
DAMODARANDamu <DAMODARANDamu@DESKTOP-IER2FA9.in.ibm.com>
2017-08-21 16:31:51 +0530

    Damu's first java code

---
+++ micro1/src/main/java/com/damu/micro1/Micro1Application.java
@@ -0,0 +1,14 @@
+package com.damu.micro1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class Micro1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Micro1Application.class, args);
+	}
+}
